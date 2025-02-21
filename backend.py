d40f099a13e29ad5f6f24bce7bde7779e7d9b274
Vinicius Alves Camilo Beltrão <vinicius.beltrao@hrgama.com>
2025-02-21 12:28:36 -0300

    Corrigindo caminho dos arquivos estáticos

--- backend.py
+++ backend.py
@@ -132,4 +132,6 @@ def match_process(user_message):
     return None
  
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Usa a porta do Render, se disponível
+    app.run(debug=True, host='0.0.0.0', port=port)
